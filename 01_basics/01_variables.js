78c960252a1f81851800aa87f5c63fe9a5c4b5c3
ha3sh <144370932+ha3sh@users.noreply.github.com>
2025-01-06 10:04:23 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 2234785
+let accountEmail ="ram123.com"
+var accountPassword = "45282937"      //don't use var.
+accountCity = "Jaipur"
+let accountState ;
+
+//accountId = 47647
+//accountEmail= "hrsh0123.com"
+accountPassword="9763537540"
+accountCity= "Delhi"
+
+console.log(accountId);  
+
+
+/* prefer not to use var
+    because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
