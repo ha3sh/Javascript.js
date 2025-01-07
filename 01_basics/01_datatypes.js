8d4c48be2858cd4663736951e37cbf7dddbc14e9
ha3sh <144370932+ha3sh@users.noreply.github.com>
2025-01-07 05:12:33 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" ;  // it treats all JS code as newer version 
+
+// alert(3+3)  // we are using nodejs , not browser
+
+console.log(3+6);     //code readability should be high
+console.log("harsh");
+
+let name = "harsh"             //string datatype
+let age = 20                   //number datatype
+let isLoggedIn = false         //boolean
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+
+
+// object
+
+
+console.log(typeof isLoggedIn);
+console.log(typeof null);      //Output of null is Object and o/p of undefined is undefined
